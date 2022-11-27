43270fe7d996012b919f2476e86e146820a251bc
LeKAKiD <lekakid@kakao.com>
2022-11-27 16:50:28 +0900

    연속 저장 기능 구현

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ function App() {
   const [jsonError, setJsonError] = useState(false);
   const [open, setOpen] = useState(false);
   const [recordTime, setRecordTime] = useState({ to: 0, from: 0 });
+  const [dataList, setDataList] = useState([]);
+
   const textElement = useRef();
   const isReverseInputElement = useRef();
   const alertTimer = useRef();
@@ -33,28 +35,51 @@ function App() {
     setJsonError(false);
   }, []);
 
+  const handleStack = useCallback(() => {
+    const stringJSON = textElement.current.value;
+    const reversed = isReverseInputElement.current.checked;
+
+    let {
+      data: { battle_list: data },
+    } = JSON.parse(stringJSON);
+
+    if (reversed) {
+      data.reverse();
+      setDataList((prev) => [...data, ...prev]);
+    } else {
+      data = reversed ? data.reverse() : data;
+      setDataList((prev) => [...prev, ...data]);
+    }
+    textElement.current.value = '';
+  }, []);
+
   const handleConvert = useCallback(async () => {
     try {
-      const stringJSON = textElement.current.value;
-      const reversed = isReverseInputElement.current.checked;
-
-      let {
-        data: { battle_list: data },
-      } = JSON.parse(stringJSON);
-
-      data = reversed ? data.reverse() : data;
       const time = {};
-      const text = data
+      let lastDate = Math.floor((dataList[0].battle_end_time + 14400) / 86400);
+      const text = dataList
         .map((d, i) => {
           if (i === 0) time.from = Number(d.battle_end_time) * 1000;
-          if (i === data.length - 1) time.to = Number(d.battle_end_time) * 1000;
+          if (i === dataList.length - 1)
+            time.to = Number(d.battle_end_time) * 1000;
 
-          const isCarried = data
+          const isCarried = dataList
+            .filter((e, j) => j < i)
+            .filter((e, j) => e.target_viewer_id === d.target_viewer_id)
             .filter(
-              (e, j) => j < i && e.target_viewer_id === d.target_viewer_id
+              (e, j) =>
+                Math.floor((e.battle_end_time + 14400) / 86400) ===
+                Math.floor((d.battle_end_time + 14400) / 86400)
             )
             .some((e) => has3OverSameUnit(d.units, e.units));
-          return `${d.order_num}\t${d.user_name}\t${d.total_damage}\t${isCarried}`;
+
+          const date = Math.floor((d.battle_end_time + 14400) / 86400);
+          const isDateOver = date > lastDate;
+          lastDate = date;
+          const dateDivider = '\t-\t\tfalse\n';
+          return `${isDateOver ? dateDivider : ''}${d.order_num}\t${
+            d.user_name
+          }\t${d.total_damage}\t${isCarried}`;
         })
         .join('\n');
 
@@ -76,7 +101,7 @@ function App() {
 
       console.error(error);
     }
-  }, []);
+  }, [dataList]);
 
   const handleClose = useCallback(() => {
     setOpen(false);
@@ -99,19 +124,32 @@ function App() {
             onFocusCapture={handleFocusCapture}
           />
           <Grid container sx={{ mt: 1 }}>
-            <Grid item xs={4}></Grid>
-            <Grid item xs={4} sx={{ textAlign: 'center' }}>
+            <Grid item xs={4}>
+              <Typography>{`저장된 데이터 수: ${dataList.length}`}</Typography>
+            </Grid>
+            <Grid item xs={2} sx={{ textAlign: 'center' }}>
               <Button
                 variant="contained"
                 sx={{ mx: 'auto' }}
+                onClick={handleStack}
+              >
+                스택
+              </Button>
+            </Grid>
+            <Grid item xs={2} sx={{ textAlign: 'center' }}>
+              <Button
+                variant="contained"
+                sx={{ mx: 'auto' }}
+                disabled={dataList.length === 0}
                 onClick={handleConvert}
               >
-                변환 후 복사
+                변환
               </Button>
             </Grid>
             <Grid item xs={4} sx={{ pr: 1, textAlign: 'right' }}>
               <FormControlLabel
                 inputRef={isReverseInputElement}
+                disabled={dataList.length > 0}
                 control={<Checkbox defaultChecked />}
                 label="순서 뒤집기"
                 labelPlacement="start"
